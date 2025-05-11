341d22994ab0ae430d4f81309e3afad4bfe2de05
Thiago De Mattia Scoti <thiago_de_mattia_scoti@outlook.com>
2025-05-11 00:37:18 -0300

    Configurações do servidor

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { Request, Response } from 'express'
+
+const app = express()
+app.use(express.json())
+app.listen(8080, ()=>{
+    console.log("Servidor rodando na porta 8080")
+})
