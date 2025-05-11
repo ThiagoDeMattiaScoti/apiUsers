26d78c935a3f725bd2b72b208b43608cfca0bc51
Thiago De Mattia Scoti <thiago_de_mattia_scoti@outlook.com>
2025-05-11 12:48:31 -0300

    Projeto buildado da .dist

--- dist/index.js
+++ dist/index.js
@@ -7,14 +7,48 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.listen(8080, () => {
-    console.log('Server funcionando na porta 8080');
+    console.log("Servidor rodando na porta 8080");
 });
 const users = [];
 app.post('/user', (req, res) => {
-    const { nome } = req.body;
-    users.push(nome);
-    res.status(201).json({ message: "Usuário criado com sucesso", nome });
+    const { name } = req.body;
+    if (!name)
+        res.status(400).json({ message: "Usuário precisa ter um nome para ser criado" });
+    const nextId = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+    const newUser = { id: nextId, name };
+    res.status(201).json({ message: "Usuário criado com sucesso", newUser });
 });
 app.get('/user', (req, res) => {
+    if (users.length === 0)
+        res.status(404).json({ message: "Não existem usuários para consulta" });
     res.status(200).json(users);
+});
+app.get('/user/:id', (req, res) => {
+    if (users.length === 0)
+        res.status(404).json({ message: "Não existem usuários para consulta" });
+    const { id } = req.params;
+    const indexUser = users.findIndex(u => u.id === parseInt(id));
+    if (indexUser < 0)
+        res.status(404).json({ message: "Não encontrado nenhum usuário com esse ID" });
+    const user = users[indexUser];
+    res.status(200).json(user);
+});
+app.post('/user/:id', (req, res) => {
+    const { id } = req.params;
+    const indexUser = users.findIndex(u => u.id === parseInt(id));
+    if (indexUser < 0)
+        res.status(404).json({ message: "Não encontrado nenhum usuário com esse ID" });
+    const { name } = req.body;
+    if (!name)
+        res.status(400).json({ message: "Usuário precisa ter um nome para ser criado" });
+    users[indexUser].name = name;
+    res.status(200).json({ message: "Usuário alterado com sucesso", name });
+});
+app.delete('/user/:id', (req, res) => {
+    const { id } = req.params;
+    const indexUser = users.findIndex(u => u.id === parseInt(id));
+    if (indexUser < 0)
+        res.status(404).json({ message: "Não encontrado nenhum usuário com esse ID" });
+    users.splice(indexUser, 1);
+    res.status(200).json({ message: "Usuário excluído com sucesso" });
 });
