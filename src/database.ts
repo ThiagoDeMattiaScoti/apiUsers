cc75514f81a57c42eece9e91911ce3b5c7b51f5c
Thiago De Mattia Scoti <thiago_de_mattia_scoti@outlook.com>
2025-05-14 22:24:46 -0300

    Adicinado rota para criar USERS administradores & adicinado verificação para somente poder alterar users se tiver credenciais de administrador

--- src/database.ts
+++ src/database.ts
@@ -1,23 +1,10 @@
-import { Pool } from 'pg'
+import { Pool } from "pg";
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
     database: 'crud_usuarios',
+    user: 'postgres',
     password: 'admin',
     port: 8888
 })
-
-async function testarConexao(){
-    try{
-        const client = await pool.connect()
-        console.log('Conexão com o PostgreSQL estabelecida!')
-        client.release()
-    } catch (err) {
-        console.log('Erro ao se conectar ao PostgreSQL:', err)
-    }
-}
-
-testarConexao()
 
 export default pool
